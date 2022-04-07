c4c01801e77223291079ea4dd43a9652fa239293
Rohan-Harish <rohan.harish@gmail.com>
2022-04-06 21:04:44 -0400

    Starter Files

---
+++ kwcsite/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import React from 'react';
+
+
+/**
+ * This is the main funcion that will handle the Routing for the Kids Who Code Website.
+ * To learn more about the project, and to get involved contact rh388@njit.edu
+ * 
+ */
+
+class App extends React.Component{
+  constructor(){}
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
